9187ba668b187d630886120dc4d437f1874695d8
Vasco Pereira <vascop_98@hotmail.com>
2019-07-08 21:56:25 +0100

    Heroku Commit

--- models/db.js
+++ models/db.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     var mongoose = require('mongoose');
-    var url = 'mongodb://Testing:traquinas21@ds249137.mlab.com:49137/heroku_26fwbx2t';
+    require('dotenv').config();
+    var user = process.env.USER;
+    var pass = process.env.PASS;
+    var url = `mongodb://${user}:${pass}@ds249137.mlab.com:49137/heroku_26fwbx2t`;
 
     mongoose.connect(url, {useNewUrlParser: true}, function() {
         console.log('Connected to Database.');
