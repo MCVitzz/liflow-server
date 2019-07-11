27f80364c29dd57a7dcfea07cbed76ec3e928c50
Vasco Pereira <vascop_98@hotmail.com>
2019-07-11 17:38:11 +0100

    Added find all users route

--- routes/users.js
+++ routes/users.js
@@ -9,10 +9,17 @@ module.exports = function(app) {
 
     app.get('/users/get', function(req, res) {
         var user = req.query.user;
-        User.findOne({'Username': user}, function(err, docs) {
-            if(err)
-                throw err;
-            res.json(docs);
-        });
+        if(user)
+            User.findOne({'Username': user}, function(err, doc) {
+                if(err)
+                    throw err;
+                res.json(doc);
+            });
+        else
+            User.find(function(err, docs) {
+                if(err)
+                    throw err;
+                res.json(docs);
+            });
     });
 };
